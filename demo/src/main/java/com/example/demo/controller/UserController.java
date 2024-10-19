d0758617353400a4462374203c4e93befb3662cf
Johny Lucio <johnylucio7@gmail.com>
2024-10-19 16:47:54 -0300

    [Ex 004] - Implemented pagination support for User Entity

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.User;
@@ -22,34 +24,35 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/users")
 public class UserController {
 	private final UserService userService;
-	
+
 	@Autowired
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
-	
+
 	@GetMapping
-	public List<User> getAllUsers(){
-		return userService.getAllUsers();
+	public Page<User> getAllUsers(@RequestParam(name = "page", defaultValue = "0") int page,
+			@RequestParam(name = "size", defaultValue = "10") int size) {
+		return userService.getAllUsers(page, size);
 	}
-	
+
 	@GetMapping("/{id}")
 	public User getUserById(@PathVariable("id") Long id) {
 		return userService.getUserById(id);
 	}
-	
+
 	@PostMapping
 	public User createUser(@Valid @RequestBody User user) {
 		return userService.createUser(user);
 	}
-	
+
 	@PutMapping("/{id}")
 	public User updateUser(@PathVariable("id") Long id, @RequestBody User userDetails) {
 		return userService.updateUser(id, userDetails);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id){
+	public ResponseEntity<Void> deleteUser(@PathVariable("id") Long id) {
 		userService.deleteUser(id);
 		return ResponseEntity.noContent().build();
 	}
--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -3,6 +3,10 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.User;
@@ -18,8 +22,11 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
-	public List<User> getAllUsers() {
-		return userRepository.findAll();
+	public Page<User> getAllUsers(int page, int size) {
+
+		Pageable paging = PageRequest.of(page, size);
+
+		return userRepository.findAll(paging);
 	}
 
 	public User getUserById(Long id) {
